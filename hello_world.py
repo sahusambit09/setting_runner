8586b0440d52f5e5e4c237b907c90ba56ccfa10d
sahusambit09 <sahusambit09@gma>
2021-05-15 16:45:22 +0530

    first_commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+import pprint
+import json
+import django
+# pprint.pprint("hello world")
+print(      "      hello world!!!"                                )
+print("something does not add up fizzzzzzzzzzyyyyyyyy!!     !")
+
+
